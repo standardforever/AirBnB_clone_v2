f09e08fc3e90668f97bfd44df44c35fd8d22081e
kusimosemiu@gmail.com <standard.forever123@gmail.com>
2022-11-13 23:44:03 -0500

    updated

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Displaying object from database
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """Close session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """return the list of state"""
+    state = storage.all(State).values()
+    return(render_template("7-states_list.html", state_list=state))
